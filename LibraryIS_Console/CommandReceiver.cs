8e3e21509c791680ca015cb64dc5557a9b55fe98
Pavel Drabek <pavel@craneballs.com>
2015-12-31 12:00:29 +0100

    console app

---
+++ LibraryIS_Console/CommandReceiver.cs
@@ -0,0 +1,54 @@
+﻿using LIS.Entities;
+using LIS.Services;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryIS_Console
+{
+    public class CommandReceiver
+    {
+        BookService bookService;
+        BorrowService borrowService;
+
+        public CommandReceiver()
+        {
+            bookService = new BookService();
+            borrowService = new BorrowService();
+        }
+
+        public void Start()
+        {
+            string command = "";
+            do {
+                command = Console.ReadLine();
+
+                if (command.Equals("books")) {
+                    ShowBookList();
+                } else if (command.Equals("borrows")) {
+                    ShowBorrowList();
+                } else {
+                    Console.WriteLine("Unknown command. Write books or borrows to show list.");
+                }
+            } while (!command.Equals("exit"));
+        }
+
+        private void ShowBookList()
+        {
+            List<Book> books = bookService.GetAll();
+            foreach(Book b in books) {
+                Console.WriteLine(b.ToString());
+            }
+        }
+
+        private void ShowBorrowList()
+        {
+            List<Borrow> borrows = borrowService.GetAll();
+            foreach (Borrow b in borrows) {
+                Console.WriteLine(b.ToString());
+            }
+        }
+    }
+}
---
+++ LibraryIS_Console/Program.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LibraryIS_Console
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            CommandReceiver receiver = new CommandReceiver();
+            receiver.Start();
+        }
+    }
+}
